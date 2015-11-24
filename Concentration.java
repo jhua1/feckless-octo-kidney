23742fb46ca2dc4ba8eef04ce85660834fae7579
wuruochong <rwu3@stuy.edu>
2015-11-23 19:19:54 -0500

    Populate done

--- Concentration.java
+++ Concentration.java
@@ -6,18 +6,49 @@ public class Concentration {
     //instance variables
     private Tile[][] _board;     //storage for 4x4 grid of Tiles.
     private int _numberFaceUp;   //count of Tiles with faces visible
-    private String[] _words;     //list of unique Strings used for Tile vals
+    private String[] _words = {"1","2","3","4","5","6","7","8"};     //list of unique Strings used for Tile vals
     private static int _numRows = 4;
 
 
     //insert constructor and methods here
+    public Concentration(){
+	_board = new Tile[4][4];
+	_numberFaceUp = 0;
+	double count = 0;
+	//	for (Tile[] x : _board){
+	//	    for (Tile y : x){
+	//	y = new Tile(_words[(int)count]);
+	//	count += 0.5;
+	//	    }
+	//	}
+    }
 
+    public void populate(){
+	double count =0;
+	for (int x=0 ; x < _board.length ; x++){
+	    for (int y = 0 ; y< _board[x].length ; y++){
+		_board[x][y] = new Tile(_words[(int)count]);
+		count += 0.5;
+	    }
+	}
+    }
 
+    public void print() { 
+        for (Tile[] x : _board){             //For each row
+	    for (Tile y : x){          //For each int
+		System.out.print(y.toString()+" ");
+	    }
+	    System.out.print("\n");
+	}
+    }
 		
     //DO NOT MODIFY main()
     public static void main(String[] args){
-	Concentration game = new Concentration();
-	game.play();
+	Concentration test = new Concentration();
+	test.populate();
+	test.print();
+	//	Concentration game = new Concentration();
+	//	game.play();
     }
 
 }//end class Concentration
